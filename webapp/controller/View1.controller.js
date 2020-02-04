fe55ef184eba69016a05b78fe326bc9463cc607b
Özdemir Canan <canan_ozdemirr@hotmail.com>
2020-02-04 12:32:41 +0000

    init commit

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("ZTEST1.ZTEST1.controller.View1", {
+		onInit: function () {
+
+		}
+	});
+});
---
+++ webapp/test/integration/AllJourneys.js
@@ -0,0 +1,13 @@
+sap.ui.define([
+	"sap/ui/test/Opa5",
+	"./arrangements/Startup",
+	"./NavigationJourney"
+], function (Opa5, Startup) {
+	"use strict";
+
+	Opa5.extendConfig({
+		arrangements: new Startup(),
+		viewNamespace: "ZTEST1.ZTEST1.view.",
+		autoWait: true
+	});
+});
